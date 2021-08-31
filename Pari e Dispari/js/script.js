e94ca2740da415ff0ed018999af5a4d13b8994c0
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-08-31 16:10:43 +0200

    pari e dispari con validazioni

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -6,12 +6,27 @@
 
 var sceltaUtente = prompt("Pari o Dispari ?");
 
+sceltaUtente = sceltaUtente.toLowerCase();
+
+console.log(sceltaUtente);
+
+//while(!(sceltaUtente == "pari") && !(sceltaUtente == "dispari") ) {
+//    sceltaUtente = prompt("Hai sbagiato a scrivere! Pari o Dispari?");
+//}
+
+while(sceltaUtente != "pari" && sceltaUtente != "dispari" ) {
+    sceltaUtente = prompt("Hai sbagiato a scrivere! Pari o Dispari?");
+}
+
 var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+while( !(numeroUtente > 0) || !(numeroUtente < 6) || isNaN(numeroUtente)){
+    numeroUtente = parseInt(prompt("Hai sbagliato Inserisci un numero da 1 a 5"));
+}
 
 function numeroRandom(min, max) {
 
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  
 }
 
 var totale = numeroUtente + numeroRandom(1, 5);
@@ -28,10 +43,10 @@ function pariOdispari(somma) {
 }
 
 if(sceltaUtente == "pari" && pariOdispari(totale) == true){
-    alert("hai vinto");
+    alert("HAI VINTO");
 } else if (sceltaUtente == "dispari" && pariOdispari(totale) == false ) {
-    alert("hai vinto");
+    alert("HAI VINTO");
 } else { 
-    alert("hai perso");
+    alert("BANGO VINCE!");
 }
 
